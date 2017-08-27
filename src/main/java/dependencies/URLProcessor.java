9dccffc9605a91839116123d1e9ae0eaaabb39f1
Andrew Black <redbudger@honeybudgers.ml>
2017-08-27 09:13:57 +0300

    second commit

---
+++ src/main/java/dependencies/URLProcessor.java
@@ -0,0 +1,13 @@
+package dependencies;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class URLProcessor {
+	
+	public static void process(String URL) {
+		if(StringUtils.isEmpty(URL)) {
+			throw new IllegalArgumentException("no url provided");
+		}
+	}
+
+}
